84700e7237c575cac078137697570fe3bd15b9ce
testsecondnugalm <test2ndnugalm2013@outlook.com>
2012-12-18 16:02:57 -0300

    prueba desde notepad con TortoiseGit

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,6 +10,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("Hola ALM");
+            Console.WriteLine("Prueba con Notepad");
             Console.ReadLine();
         }
     }
